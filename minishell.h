c91b06efef12afddcd373a9064cc29d719e86721
IBF-i3 <iria.benavente@gmail.com>
2026-02-14 19:13:04 +0100

    added function free_split declaration

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: tkulivar <tkulivar@student.42madrid.com>   #+#  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025-12-06 10:51:15 by tkulivar          #+#    #+#             */
-/*   Updated: 2026/02/14 04:18:59 by ibenaven         ###   ########.fr       */
+/*   Updated: 2026/02/14 13:36:47 by ibenaven         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,6 @@
 # include "libft.h"
 
 char	*build_cmd_path(char **split_paths, char *cmd);
+void	*free_split(char **array);
 
 #endif
