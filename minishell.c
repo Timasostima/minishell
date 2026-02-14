d88be990bd55c08c12a7dbc3d98bbe80a72dfaad
IBF-i3 <iria.benavente@gmail.com>
2026-02-14 19:16:54 +0100

    added missing free and free_split functions to avoid leaks

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: ibenaven <ibenaven@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/18 16:53:36 by ibenaven          #+#    #+#             */
-/*   Updated: 2026/02/14 04:35:00 by ibenaven         ###   ########.fr       */
+/*   Updated: 2026/02/14 17:36:36 by ibenaven         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,10 @@ int	main(int argc, char *argv[], char *envp[])
 			break;
 		}
 		if (input[0] == '\0')
+		{
+			free(input);
 			continue;
+		}
 		add_history(input);
 		if (ft_strcmp(input, "exit") == 0)
 			break;
@@ -44,6 +47,8 @@ int	main(int argc, char *argv[], char *envp[])
 		if (pid == -1)
 		{
 			perror("fork() error");
+			free (input);
+			rl_clear_history();
 			exit(EXIT_FAILURE);
 		}
 		else if (pid == 0)
@@ -53,16 +58,19 @@ int	main(int argc, char *argv[], char *envp[])
 			split_paths = ft_split(path, ':');
 			cmd_path = build_cmd_path(split_paths, split_argvs[0]);	
 			execve(cmd_path, split_argvs, envp);
-			free(input);
+			free_split(split_argvs);
+			free_split(split_paths);
+			free (input);
+			rl_clear_history();
 			exit(EXIT_FAILURE);
 		}
 		else
 		{
 			printf("%jd\n", (intmax_t) pid);
 			waitpid(pid, &status, 0);
+			free (input);
 		}	
 	}
-	free (input);
 	rl_clear_history();
 	return (0);
 }
